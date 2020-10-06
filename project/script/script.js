1f6e0b6ab09dd4d14b146b4d1340d5c847db446b
NicholasHD <nicholashadi2614@gmail.com>
2020-10-06 09:29:02 +0700

    Some Form Validation and Cancel Booking Button

---
+++ project/script/script.js
@@ -0,0 +1,125 @@
+$(function () {
+    var $password = $(".form-control[id='exampleInputPassword1']");
+    var $passwordAlert = $(".password-alert");
+    var $requirements = $(".requirements");
+    var leng, bigLetter, num;
+    var $leng = $(".leng");
+    var $bigLetter = $(".big-letter");
+    var $num = $(".num");
+    var numbers = "0123456789";
+
+    $requirements.addClass("wrong");
+    $password.on("focus", function(){$passwordAlert.show();});
+
+    $password.on("input blur", function (e) {
+        var el = $(this);
+        var val = el.val();
+        $passwordAlert.show();
+
+        if (val.length < 8) {
+            leng = false;
+        }
+        else if (val.length > 7) {
+            leng=true;
+        }
+
+        if(val.toLowerCase()==val){
+            bigLetter = false;
+        }
+        else{bigLetter=true;}
+        
+        num = false;
+        for(var i=0; i<val.length;i++){
+            for(var j=0; j<numbers.length; j++){
+                if(val[i]==numbers[j]){
+                    num = true;
+                }
+            }
+        }
+        
+
+        console.log(leng, bigLetter, num);
+        
+        if(leng==true&&bigLetter==true&&num==true){
+            $(this).addClass("valid").removeClass("invalid");
+            $requirements.removeClass("wrong").addClass("good");
+            $passwordAlert.removeClass("alert-warning").addClass("alert-success");
+        }
+        else
+        {
+            $(this).addClass("invalid").removeClass("valid");
+            $passwordAlert.removeClass("alert-success").addClass("alert-warning");
+
+            if(leng==false){$leng.addClass("wrong").removeClass("good");}
+            else{$leng.addClass("good").removeClass("wrong");}
+
+            if(bigLetter==false){$bigLetter.addClass("wrong").removeClass("good");}
+            else{$bigLetter.addClass("good").removeClass("wrong");}
+
+            if(num==false){$num.addClass("wrong").removeClass("good");}
+            else{$num.addClass("good").removeClass("wrong");}
+        }
+        
+        
+        if(e.type == "blur"){
+                $passwordAlert.hide();
+            }
+    });
+});
+
+$(document).ready(function() {
+    $('#autoWidth').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('#autoWidth').removeClass('cS-hidden');
+        } 
+    });  
+  });
+
+function myFunction(){
+    alert("This place doesn't need ticket");
+}
+
+$(document).ready(function() {
+      
+    // navigation click actions 
+    $('.scroll-link').on('click', function(event){
+        event.preventDefault();
+        var sectionID = $(this).attr("data-id");
+        scrollToID('#' + sectionID, 750);
+    });
+    // scroll to top action
+    $('.scroll-top').on('click', function(event) {
+        event.preventDefault();
+        $('html, body').animate({scrollTop:0}, 'slow');         
+    });
+    // mobile nav toggle
+    $('#nav-toggle').on('click', function (event) {
+        event.preventDefault();
+        $('#main-nav').toggleClass("open");
+    });
+});
+// scroll function
+function scrollToID(id, speed){
+    var offSet = 0;
+    var targetOffset = $(id).offset().top - offSet;
+    var mainNav = $('#main-nav');
+    $('html,body').animate({scrollTop:targetOffset}, speed);
+    if (mainNav.hasClass("open")) {
+        mainNav.css("height", "1px").removeClass("in").addClass("collapse");
+        mainNav.removeClass("open");
+    }
+}
+if (typeof console === "undefined") {
+    console = {
+        log: function() { }
+    };
+}
+
+jQuery(document).ready(function(){
+    $('.button').click(function(){
+      $('.one').addClass('oneani');
+      $('.two').addClass('twoani');
+    });
+  })
