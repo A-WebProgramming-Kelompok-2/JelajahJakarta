425760a497f7efc8a7d5999000e38bd8ae7d2864
unknown <dellya110701@gmail.com>
2020-11-20 12:51:51 +0700

    start backend

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express'),
+    app = express(),
+    bodyparser= require(body-parser);
+
+app.set('view engine','ejs');
+
+app.use(express.static('public'));
+app.use(express.static('public'))
