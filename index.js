fd69b76a9efc42ced6f48743b2be0e428522e0a8
unknown <dellya110701@gmail.com>
2020-11-20 23:05:49 +0700

    create routing

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express'),
     app = express(),
-    bodyparser= require(body-parser);
+    bodyparser= require('body-parser');
 
 app.set('view engine','ejs');
 
 app.use(express.static('public'));
-app.use(express.static('public'))
+app.use(bodyparser.urlencoded());
+
+const port = process.env.port || 8000;
+
+//routes
+const indexRoutes = require('./routes/index');
+app.use('/', indexRoutes);
+
+app.listen(port);
+console.log(`successfully run on ${port}`);
