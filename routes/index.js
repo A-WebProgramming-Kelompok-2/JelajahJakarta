81e32a91ffef85118cda156178a32de376af447c
yongkysaputra <70826657+yongkysaputra@users.noreply.github.com>
2020-11-26 12:11:58 +0700

    set popular place price

---
+++ routes/index.js
@@ -0,0 +1,48 @@
+const express= require('express'),
+    router = express.Router();
+
+    router.get('/',(req,res)=>{
+        res.render('pages/index');
+    });
+    
+    router.get('/popular',(req,res)=>{
+        res.render('pages/popular');
+    });
+    
+    router.get('/amusement-water-park',(req,res)=>{
+        res.render('pages/amusement-water park');
+    });
+    
+    router.get('/beach-island',(req,res)=>{
+        res.render('pages/beach-island');
+    });
+    
+    router.get('/culinary',(req,res)=>{
+        res.render('pages/culinary');
+    });
+    
+    router.get('/experience',(req,res)=>{
+        res.render('pages/experience');
+    });
+    
+    router.get('/jakarta-history-places',(req,res)=>{
+        res.render('pages/jakarta-history-places');
+    });
+    
+    router.get('/lake-garden',(req,res)=>{
+        res.render('pages/lake-garden');
+    });
+    
+    router.get('/tips',(req,res)=>{
+        res.render('pages/tips');
+    });
+    
+    router.get('/zoo',(req,res)=>{
+        res.render('pages/zoo');
+    });
+    
+    router.get('/mybooking',(req,res)=>{
+        res.render('pages/mybooking');
+    });
+
+module.exports = router;
